1c5ec0869d88e34c9b1b1c350b4af6001ca5a9c0
wuhantao <wuhantao1216@gmail.com>
2021-12-25 00:38:14 +0800

    frist commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let { createClient } = require("redis");
+
+(async () => {
+  const client = createClient();
+
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+
+  await client.set("key", "value");
+  const value = await client.get("key");
+  console.log(value);
+  client.quit();
+})();
